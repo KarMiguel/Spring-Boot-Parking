11afbe4d1239a1afb466cbf64855c875961e8158
KarMiguel <carlosmiguel.dsa12@gmail.com>
2024-03-09 22:14:58 -0300

    feat: config timezone locale

---
+++ src/main/java/io/github/KarMiguel/parkingapi/config/SpringTimezoneConfig.java
@@ -0,0 +1,15 @@
+package io.github.KarMiguel.parkingapi.config;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.TimeZone;
+
+@Configuration
+public class SpringTimezoneConfig {
+
+    @PostConstruct
+    public void timezoneConfig(){
+        TimeZone.setDefault(TimeZone.getTimeZone(("America/Sao_Paulo")));
+    }
+}
