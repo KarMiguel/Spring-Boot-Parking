a5e153261d16e20387cb798cbf29dc9a752d918e
KarMiguel <carlosmiguel.dsa12@gmail.com>
2024-03-10 23:18:35 -0300

    feat: dto login

---
+++ src/main/java/io/github/KarMiguel/parkingapi/web/dto/UserLoginDTO.java
@@ -0,0 +1,26 @@
+package io.github.KarMiguel.parkingapi.web.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class UserLoginDTO {
+
+    @NotBlank
+    @Email( regexp = "^[a-z0-9.+-]+@[a-z0-9.-]+\\.[a-z]{2,}$",message = "formato email inválido.")
+    private String username;
+
+    @NotBlank
+    @Size(min = 6,max = 6)
+    private String password;
+
+}
