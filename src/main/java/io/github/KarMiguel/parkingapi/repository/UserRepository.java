d7ca2967297f57aee036ea93367b1a9e5f604935
KarMiguel <carlosmiguel.dsa12@gmail.com>
2024-03-10 23:19:13 -0300

    feat: add findByUsername

--- src/main/java/io/github/KarMiguel/parkingapi/repository/UserRepository.java
+++ src/main/java/io/github/KarMiguel/parkingapi/repository/UserRepository.java
@@ -2,8 +2,15 @@ package io.github.KarMiguel.parkingapi.repository;
 
 import io.github.KarMiguel.parkingapi.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User,Long> {
 
+    Optional<User> findByUsername(String username);
+
+    @Query("Select u.Role from User u where u.username like:username")
+    User.Role findRoleByUsername(String username);
 }
