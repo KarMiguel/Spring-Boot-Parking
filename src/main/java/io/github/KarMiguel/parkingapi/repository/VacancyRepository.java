e6b35ec3eb3abb52ed8e4c3fae4033ee63420a04
KarMiguel <carlosmiguel.dsa12@gmail.com>
2024-03-15 20:32:21 -0300

    feat: created reoisitory vacancy

---
+++ src/main/java/io/github/KarMiguel/parkingapi/repository/VacancyRepository.java
@@ -0,0 +1,11 @@
+package io.github.KarMiguel.parkingapi.repository;
+
+import io.github.KarMiguel.parkingapi.entity.Vacancy;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface VacancyRepository extends JpaRepository<Vacancy,Long> {
+
+    Optional<Vacancy> findByCode(String code);
+}
---
+++ src/main/java/io/github/KarMiguel/parkingapi/web/dto/mapper/VacancyMapper.java
@@ -0,0 +1,20 @@
+package io.github.KarMiguel.parkingapi.web.dto.mapper;
+
+
+import io.github.KarMiguel.parkingapi.entity.Vacancy;
+import io.github.KarMiguel.parkingapi.web.dto.vacancy.VacancyCreatedDTO;
+import io.github.KarMiguel.parkingapi.web.dto.vacancy.VacancyResponseDTO;
+import lombok.*;
+import org.modelmapper.ModelMapper;
+
+@NoArgsConstructor(access =  AccessLevel.PRIVATE)
+public class VacancyMapper {
+
+    public  static Vacancy toVacancy(VacancyCreatedDTO dto){
+        return  new ModelMapper().map(dto,Vacancy.class);
+    }
+
+    public  static VacancyResponseDTO toDto(Vacancy vacancy){
+        return  new ModelMapper().map(vacancy,VacancyResponseDTO.class);
+    }
+}
