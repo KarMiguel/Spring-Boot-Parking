930358083a1a26d19826c9d88589dddb35ba99ef
KarMiguel <carlosmiguel.dsa12@gmail.com>
2024-03-15 15:16:07 -0300

    feat: created modeling, services and client repository and dto

--- src/main/java/io/github/KarMiguel/parkingapi/entity/Client.java
+++ src/main/java/io/github/KarMiguel/parkingapi/entity/Client.java
@@ -1,9 +1,69 @@
 package io.github.KarMiguel.parkingapi.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Entity
+@Table(name = "client")
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Client implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false,length = 100)
+    private String name;
+
+    @Column(nullable = false,unique = true,length = 11)
+    private String cpf;
+
+    @OneToOne
+    @JoinColumn(nullable = false,name = "id_user")
+    private Users user;
+
+    @CreatedDate
+    @Column(name = "date_created")
+    private LocalDateTime dateCreated;
+
+    @LastModifiedDate
+    @Column(name = "date_modification")
+    private  LocalDateTime dateModification;
+
+    @CreatedBy
+    @Column(name = "created_by")
+    private String createdBy;
+
+    @LastModifiedBy
+    @Column(name = "modified_by")
+    private  String modifiedBy;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Client client = (Client) o;
+        return Objects.equals(id, client.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
