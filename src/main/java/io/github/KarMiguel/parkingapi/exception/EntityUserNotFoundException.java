958f007ad437ef149451ceb901fdde663097639d
KarMiguel <carlosmiguel.dsa12@gmail.com>
2024-03-17 23:13:25 -0300

    feat: change name class

--- src/main/java/io/github/KarMiguel/parkingapi/exception/EntityUserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package io.github.KarMiguel.parkingapi.exception;
-
-public class EntityUserNotFoundException extends RuntimeException {
-    public EntityUserNotFoundException(String message) {
-        super(message);
-    }
-}
